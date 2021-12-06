aa380e4c09cd9c96cc8ef47c38ac888a711f113b
Alexandru <alexandru.purcea@softvision.com>
2021-12-06 17:41:20 +0200

    feat: add home screen base

---
+++ components/Controls.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import LottieView from "lottie-react-native";
+
+export function Controls() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.group}>
+        <Text style={styles.title}>Customise your</Text>
+
+        <Text style={styles.subtitle}>Christmas Tree</Text>
+      </View>
+
+      <LottieView
+        source={require("../assets/images/merryChristmas")}
+        style={{
+          width: "100%",
+        }}
+        autoPlay
+        loop
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "35%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "rgba(255, 255, 255, 0.77)",
+    height: "100%",
+  },
+  group: {
+    width: '100%',
+  },
+  title: {
+    marginTop: 25,
+    color: "black",
+    fontFamily: "rochester-regular",
+    fontSize: 52,
+    width: '100%',
+    textAlign: 'center'
+  },
+  subtitle: {
+    color: "black",
+    fontFamily: "rochester-regular",
+    fontSize: 62,
+    width: '100%',
+    textAlign: 'center'
+  },
+});
